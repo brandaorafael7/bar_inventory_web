9bb77ec06735b3d6378d02178636de5da9488ea8
brandaorafael7 <164428040+brandaorafael7@users.noreply.github.com>
2026-08-20 20:49:48 -0300

    fix: use VITE_API_URL envoriment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
 });
 
-// Anexa automaticamente o token JWT salvo em todas as requisições
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('@bar_token');
+  const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
